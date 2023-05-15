c295e4644f3e81eab44d7226f741352e54622ca1
Jose Arenado <vidalstat@gmail.com>
2023-05-15 19:21:07 -0400

    initial commit

---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+module.exports = {
+	title: "Dad Sends Sweaters",
+	url: "https://josearenado.com/",
+	language: "en",
+	description: "I am writing about my experiences as a naval navel-gazer.",
+	author: {
+		name: "José Arenado",
+		email: "me@josearenado.com",
+		url: "https://josearenado.com/about/"
+	}
+}
---
+++ src/index.md
@@ -0,0 +1,12 @@
+---
+title: Title
+layout: index.njk
+
+---
+
+# Hello
+
+<div class="card">
+  <h5>{{ title }}</h5>
+  <p>{{ metadata.description }}</p>
+</div>
